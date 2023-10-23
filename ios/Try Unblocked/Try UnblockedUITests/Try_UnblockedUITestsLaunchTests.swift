0920a0e8280ee7bacc8b496acf7b1ac0de96f2d4
cancelself <cancelself@icloud.com>
2023-10-22 22:15:55 -0700

    add ios client and add loginview to web

---
+++ ios/Try Unblocked/Try UnblockedUITests/Try_UnblockedUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Try_UnblockedUITestsLaunchTests.swift
+//  Try UnblockedUITests
+//
+//  Created by cancelself on 10/22/23.
+//
+
+import XCTest
+
+final class Try_UnblockedUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,16 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
 
+function Login() {
+  return (
+    <form method="post" action="/api/login">
+      <input type="text" name="username" />
+      <input type="password" name="password" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
 export default function Home() {
   const count = useSignal(3);
   return (
