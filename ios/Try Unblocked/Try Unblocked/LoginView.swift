40d31681a44366dfaefef5b7ec8511879bacb78b
cancelself <cancelself@icloud.com>
2023-10-22 22:34:40 -0700

    add android client

---
+++ ios/Try Unblocked/Try Unblocked/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Try Unblocked
+//
+//  Created by cancelself on 10/22/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ ios/Try Unblocked/Try Unblocked/StorageView.swift
@@ -0,0 +1,18 @@
+//
+//  StorageView.swift
+//  Try Unblocked
+//
+//  Created by cancelself on 10/22/23.
+//
+
+import SwiftUI
+
+struct StorageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StorageView()
+}
