3cd8d52b79842d0096aad876b6348c82d47d7856
cancelself <cancelself@icloud.com>
2023-10-30 18:54:51 -0700

    Update the home page to use the login component
    the login component calls the auth service in the backend, which then uses Auth0.

--- routes/index.tsx
+++ routes/index.tsx
@@ -23,12 +23,12 @@ export default function Home() {
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <h1 class="text-4xl font-bold">Welcome to Try Unblocked.</h1>
         <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+          Login to start.
         </p>
         <Counter count={count} />
+        <Login />
       </div>
     </div>
   );
