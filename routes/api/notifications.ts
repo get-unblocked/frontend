ad7a1fae6c4900044e7dcf3859bc00883ad51031
cancelself <332509+cancelself@users.noreply.github.com>
2023-11-13 18:46:31 -0800

    Introducing the StorageData component
    @Unblocked helped me create a better factoring of the Storage view using a StorageData React component

--- routes/api/notifications.ts
+++ routes/api/notifications.ts
@@ -1,21 +1,19 @@
-//this is a route that will be used to send notifications to the user
-//this where users go to see their notifications
-//it calls the notification api on the backend to the notifications.
-import { Router } from "express";
+import { Router } from "https://deno.land/x/oak/mod.ts";
 
-const router = Router();
+const router = new Router();
 
-router.get('/user/notifications', async (req, res) => {
-    const userId = req.user.id; // Assuming user id is available in the request
+router.get('/user/notifications', async (context) => {
+    const userId = context.state.user.id; // Assuming user id is available in the context state
 
     try {
-        const response = await fetch(`http://tryunblocked.com/notifications/status?userId=${userId}`);
-        const notifications = await response.json();
+        const response = await fetch(`http://tryunblocked.com/notifications?userId=${userId}`);
+        const data = await response.json();
 
-        res.json(notifications);
+        context.response.body = data;
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error fetching notifications' });
+        context.response.status = 500;
+        context.response.body = { message: 'Error fetching user data' };
     }
 });
 
