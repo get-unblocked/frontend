b835dce31679e2d37bc0aef7d7e03db8d109b312
cancelself <332509+cancelself@users.noreply.github.com>
2023-11-13 17:27:41 -0800

    Updated fresh.gen.ts with new routes
     "./routes/api/notifications.ts": $4,
        "./routes/api/storage.ts": $5,
        "./routes/greet/[name].tsx": $6,
        "./routes/index.tsx": $7,
        "./routes/logout.ts": $8,
        "./routes/notifications.tsx": $9,
        "./routes/storage.tsx": $10,

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,9 +6,13 @@ import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/api/login.ts";
-import * as $4 from "./routes/greet/[name].tsx";
-import * as $5 from "./routes/index.tsx";
-import * as $6 from "./routes/logout.ts";
+import * as $4 from "./routes/api/notifications.ts";
+import * as $5 from "./routes/api/storage.ts";
+import * as $6 from "./routes/greet/[name].tsx";
+import * as $7 from "./routes/index.tsx";
+import * as $8 from "./routes/logout.ts";
+import * as $9 from "./routes/notifications.tsx";
+import * as $10 from "./routes/storage.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -17,9 +21,13 @@ const manifest = {
     "./routes/_app.tsx": $1,
     "./routes/api/joke.ts": $2,
     "./routes/api/login.ts": $3,
-    "./routes/greet/[name].tsx": $4,
-    "./routes/index.tsx": $5,
-    "./routes/logout.ts": $6,
+    "./routes/api/notifications.ts": $4,
+    "./routes/api/storage.ts": $5,
+    "./routes/greet/[name].tsx": $6,
+    "./routes/index.tsx": $7,
+    "./routes/logout.ts": $8,
+    "./routes/notifications.tsx": $9,
+    "./routes/storage.tsx": $10,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
